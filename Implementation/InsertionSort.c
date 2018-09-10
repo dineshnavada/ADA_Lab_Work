c67c4182b7d55b885dd42b8dd03cf32c7e524b5a
dineshnavada <dineshkrnavada@gmail.com>
2018-09-10 18:39:02 +0530

    Some corrections

--- Implementation/InsertionSort.c
+++ Implementation/InsertionSort.c
@@ -1,42 +1,39 @@
+/*********************************
+*Author:Dinesh K R Navada        *
+*Date of execution:07 Sept 2018  *
+**********************************/
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 #include<sys/time.h>
 
-void InsertionSort(int a[], int n)
-{
+void InsertionSort(int a[], int n){
 	int i, j, v;
-	for(i=1;i<n;i++)
-	{
+	for(i=1;i<n;i++){
 		v=a[i];
 		j=i-1;
-		while(j>=0 && a[j]>v)
-		{
+		while(j>=0 && a[j]>v){
 			a[j+1]=a[j];
 			j--;
 		}
 		a[j+1]=v;
 	}
 }
-int main()
-{
+int main(){
 	int i, n;
 	printf("Enter the number of elements:");
 	scanf("%d", &n);
 	int a[n];
 	srand(time(NULL));
 	printf("\nThe unsorted elements are:");
-	for(i=0;i<n;i++)
-	{
+	for(i=0;i<n;i++){
 		a[i]=rand()%100;
 		printf("%d\t", a[i]);
 	}
 	InsertionSort(a,n);
 	printf("\nThe sorted elements are:");
 	for(i=0;i<n;i++)
-	{
 		printf("%d\t", a[i]);
-	}
 	printf("\n");
 	return 0;
 }
--- Implementation/TopologicalSorting.c
+++ Implementation/TopologicalSorting.c
@@ -1,35 +1,33 @@
+/*********************************
+*Author:Dinesh K R Navada        *
+*Date of execution:07 Sept 2018  *
+**********************************/
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
 #include<sys/time.h>
 #define MAX 10
-void topologicalSort(int a[MAX][MAX], int v)
-{
+
+void topologicalSort(int a[MAX][MAX], int v){
 	int in[MAX],out[MAX], stack[MAX];
 	int i,j,k=0, top=-1;
-	for(i=0;i<v;i++)
-	{
+	for(i=0;i<v;i++){
 		in[i]=0;
-		for(j=0;j<v;j++)
-		{
+		for(j=0;j<v;j++){
 			if(a[j][i]==1)
 				in[i]++;
 		}
 	}
-	while(1)
-	{
-		for(i=0;i<v;i++)
-		{
-			if(in[i]==0)
-			{
+	while(1){
+		for(i=0;i<v;i++){
+			if(in[i]==0){
 				stack[++top]=i;
 				in[i]=-1;
 			}
 		}
 			if(top==-1) break;
 			out[k]=stack[top--];
-			for(i=0;i<v;i++)
-			{
+			for(i=0;i<v;i++){
 				if(a[out[k]][i]==1)
 					in[i]-=1;
 			}
@@ -37,21 +35,16 @@ void topologicalSort(int a[MAX][MAX], int v)
 	}
 	printf("\nThe topological ordering is:");
 	for(i=0;i<v;i++)
-	{
 		printf("%d\t", out[i]+1);
 	}
-}
-int main()
-{
+int main(){
 	int a[MAX][MAX],v,i,j;
 	printf("Enter number of vertices:");
 	scanf("%d",&v);
 	printf("\nEnter the Adjacancy matrix:\n");
 	for(i=0;i<v;i++)
-	{
-		for(j=0;j<v;j++)
-			scanf("%d",&a[i][j]);
-	}
+			for(j=0;j<v;j++)
+				scanf("%d",&a[i][j]);
 	topologicalSort(a,v);
 	printf("\n");
 	return 0;
